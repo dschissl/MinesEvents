c3371e408173382039f8f73e75cb000e9d0ded1f
krobison <krobison@mines.edu>
2013-11-10 01:08:53 -0700

    Added a (successful) test map Marker

--- project/public/script/googleMaps.js
+++ project/public/script/googleMaps.js
@@ -1,11 +1,16 @@
+ var myMap;
+ 
+ window.onload = loadScript;
+ 
  function initialize() {
     var mapOptions = {
         center: new google.maps.LatLng(39.751244,-105.222260),
         zoom: 17,
         mapTypeId: google.maps.MapTypeId.SATELLITE
     };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
+    myMap = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	
+	testMarker();
 }
 	  
 function loadScript() {
@@ -15,4 +20,14 @@ function loadScript() {
 	document.body.appendChild(script);
 }
 
-window.onload = loadScript;
+function testMarker(){
+	var location = new google.maps.LatLng(39.751244,-105.222260);
+	
+	var markerOptions = {
+		title: "Test Event Marker",
+		map: myMap,
+		position: location
+	};
+
+	var marker = new google.maps.Marker(markerOptions);
+}
