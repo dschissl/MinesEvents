b98e52c5d65f8480956ff9d365370c897ac9a948
krobison <krobison@mines.edu>
2013-11-10 01:51:09 -0700

    Added a pop-up message in API when marker is clicked

--- project/public/script/googleMaps.js
+++ project/public/script/googleMaps.js
@@ -4,7 +4,7 @@
  
  function initialize() {
     var mapOptions = {
-        center: new google.maps.LatLng(39.751244,-105.222260),
+        center: new google.maps.LatLng(39.751244,-105.222260),	//inbetween kafadar and gugenheim
         zoom: 17,
         mapTypeId: google.maps.MapTypeId.SATELLITE
     };
@@ -26,8 +26,22 @@ function testMarker(){
 	var markerOptions = {
 		title: "Test Event Marker",
 		map: myMap,
-		position: location
+		position: location,
 	};
-
 	var marker = new google.maps.Marker(markerOptions);
+	
+	var windowOptions = {
+		content: '<h2>Cult Meeting #3</h2>'+
+			'<h4>Today we choose outfits</h4>'+
+			'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.'+
+			'Donec ullamcorper magna id lacus posuere, eu sollicitudin justo blandit. Nullam diam purus, viverra sed tortor vitae, rhoncus '+
+			'scelerisque enim. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+		maxWidth: 300
+	};
+		
+	var infoWindow = new google.maps.InfoWindow(windowOptions);
+
+	google.maps.event.addListener(marker,'click',function() {
+		infoWindow.open(myMap,marker);
+	});
 }
