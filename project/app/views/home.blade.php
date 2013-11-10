5b957963a2dfecc901c3320d31ef7ebb62690aa7
krobison <krobison@mines.edu>
2013-11-09 22:54:08 -0700

    Added home.css and pretty search bar

--- project/app/views/home.blade.php
+++ project/app/views/home.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.master')
 
+{{ HTML::style('css/home.css') }}
+
 @section('title')
     Home
 @endsection
 
 @section('content')
-    CONTENT
+	<div class="input-group" id="searchgroup">
+		<input type="text" class="form-control" id="searchbar">
+			<span class="input-group-btn">
+				<button class="btn btn-default" type="button">Go!</button>
+			</span>
+    </div>
 @endsection
