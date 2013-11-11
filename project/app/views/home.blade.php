f9f63313527f9e40d86aee4d2226ed85e9202ed8
krobison <krobison@mines.edu>
2013-11-10 18:51:51 -0700

    More dynamic sizing, as well as datepickers

--- project/app/views/home.blade.php
+++ project/app/views/home.blade.php
@@ -21,7 +21,8 @@
 		<div class="input-group filters">
 			<span class="input-group-addon">Location</span><input type="text" class="form-control" placeholder="BBW250">
 		</div>
-		<div class="bfh-datepicker"></div>
+		<span class="input-group-addon">Start Date</span><div class="bfh-datepicker filters" id="startdate" data-date="today" data-min="today"></div>
+		<span class="input-group-addon">End Date</span><div class="bfh-datepicker filters" id="enddate" data-date="today" data-min="today"></div>
 	</div>
 	<div id="map-canvas"> <!-- AIzaSyAXTdwWRRhOzQDa1lRcQ7PfKsuhV-XG78A is our API key, also used in the js call -->
 	</div>
