9c73aa201c087e85815c2ac5dbe3404d4ef24fad
krobison <krobison@mines.edu>
2013-11-10 23:05:42 -0700

    Started create event page

--- project/app/views/newevent.blade.php
+++ project/app/views/newevent.blade.php
@@ -1,9 +1,27 @@
 @extends('layouts.master')
 
+{{ HTML::style('css/newevent.css') }}
+{{ HTML::script('script/googleMaps.js'); }}
+
 @section('title')
     Create Event
 @endsection
 
 @section('content')
-    Create
+    <div class="inputpanel">
+		<div class="input-group">	<span class="input-group-addon">Name</span><input type="text" class="form-control" placeholder="Event Name">	</div>
+		<div class="input-group">	<span class="input-group-addon">Location</span><input type="text" class="form-control" placeholder="Where it is">	</div>
+		<div class="input-group">	<span class="input-group-addon"><input type="checkbox"></span><fieldset disabled><input type="text" class="form-control" placeholder="Public"></fieldset>	</div>
+		<div class="input-group">	<span class="input-group-addon">Group</span><select class="form-control">
+			<option>Group 1</option>
+			<option>Group 2</option>
+			<option>Group 3</option>
+			<option>Group 4</option>
+			<option>Group 5</option>
+		</select></div>
+		<div class="daygroup"><span class="daylabel">Start Date</span><div class="bfh-datepicker filters" id="startdate" data-date="today" data-min="today"></div>	</div>
+		<div class="daygroup"><span class="daylabel">End Date</span><div class="bfh-datepicker filters" id="enddate" data-date="today" data-min="today"></div>	</div>
+	</div>
+	<div class="mappanel">
+	</div>
 @endsection
