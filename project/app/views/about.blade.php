068d8b1f782b6dbcc4035e97dc3e5e560bac4b7c
krobison <krobison@mines.edu>
2013-11-11 00:34:33 -0700

    Added an about page

--- project/app/views/about.blade.php
+++ project/app/views/about.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.master')
 
+{{ HTML::style('css/about.css') }}
+
 @section('title')
     About
 @endsection
 
 @section('content')
-    About
+	<div class="aboutbox">
+		<h1 class="abouttext">About Us</h1>
+		<p class="abouttext">This website was created by three computer science students at the Colorado School of Mines. It allows students to view and create campus event details. The usage of Google Maps API allows students to view events by location on a campus map as well.</p>
+	</div>
 @endsection
