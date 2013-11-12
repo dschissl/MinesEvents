6fafadd9f3d0413f5285391c01482b80cedef29d
root <root@derek-UNX.(none)>
2013-11-12 04:09:14 -0700

    working on number box validation for non-supported browsers

--- project/app/views/newevent.blade.php
+++ project/app/views/newevent.blade.php
@@ -13,35 +13,100 @@
 @endsection
 
 @section('content')
-	<form action="<?php echo asset('eventcreated'); ?>" method="get">
-    <div class="inputpanel">
-		<div class="input-group">	<span class="input-group-addon">Name</span><input type="text" class="form-control" placeholder="Event Name" name="name" required>	</div>
-		<div class="input-group">	<span class="input-group-addon">Location</span><input type="text" class="form-control" placeholder="Where it is" name="location" required>	</div>
-		<div class="input-group">	<span class="input-group-addon"><input type="checkbox" name="public"></span><fieldset disabled><input type="text" class="form-control" placeholder="Public"></fieldset>	</div>
-		<div class="input-group">	<span class="input-group-addon">Group</span><select class="form-control" name="group">
-			<option>None</option>
-		</select></div>	
-		<div class="input-group">	<span class="input-group-addon">Start Date</span>	<div data-date-format="yyyy-mm-dd">	<input class="form-control" type="text" id="daystart" name="daystart"></div></div>
-		<div class="input-group">	<span class="input-group-addon">End Date</span>	<div data-date-format="yyyy-mm-dd">	<input class="form-control" type="text" id="dayend" name="dayend"></div></div>
-		<div class="input-group">	
-			<span class="input-group-addon">Start Time</span>
-			<input type="number" class="form-control" placeholder="Hour" name="hourstart" max="23" min="0" value="12" id="hourstart">
-			<input type="number" class="form-control" placeholder="Minute" name="minutestart" max="59" min="0" value="00" step="10" id="minutestart">
-		</div>
-		<div class="input-group">	
-			<span class="input-group-addon">End Time</span>
-			<input type="number" class="form-control" placeholder="Hour" name="hourend" max="23" min="0" value="12" id="hourend">
-			<input type="number" class="form-control" placeholder="Minute" name="minuteend" max="59" min="0" value="00" step="10" id="minuteend">
-		</div>
+    <form action="<?php echo asset('eventcreated'); ?>" method="get">
+        <div class="inputpanel">
+            <div class="input-group">	
+                <span class="input-group-addon">Name</span>
+                <input type="text" class="form-control" placeholder="Event Name" name="name" required="required" />	
+            </div>
+            <div class="input-group">	
+                <span class="input-group-addon">Location</span>
+                <input type="text" class="form-control" placeholder="Where it is" name="location" required="required" />	
+            </div>
+            <div class="input-group">	
+                <span class="input-group-addon">
+                    <input type="checkbox" name="public" />
+                </span>
+                <fieldset disabled>
+                    <input type="text" class="form-control" placeholder="Public" />
+                </fieldset>	
+            </div>
+            <!--<div class="input-group">	
+                <span class="input-group-addon">Group</span>
+                <select class="form-control" name="group">
+                     <option>None</option>
+                </select>
+            </div>-->
+            <div class="input-group">	
+                <span class="input-group-addon">Start Date</span>	
+                <div data-date-format="yyyy-mm-dd">	
+                    <input class="form-control" type="text" id="daystart" name="daystart" />
+                </div>
+            </div>
+            <div class="input-group">	
+                <span class="input-group-addon">End Date</span>	
+                <div data-date-format="yyyy-mm-dd">	
+                    <input class="form-control" type="text" id="dayend" name="dayend" />
+                </div>
+            </div>
+            <div class="input-group">	
+                <span class="input-group-addon">Start Time</span>
+                <input type="number" class="form-control" placeholder="Hour" name="hourstart" max="23" min="0" value="12" id="hourstart" />
+                <input type="number" class="form-control" placeholder="Minute" name="minutestart" max="59" min="0" value="00" step="10" id="minutestart" />
+                <select class="form-control" name="halfdaystart">
+                    <option selected="selected">AM</option>
+                    <option>PM</option>
+                </select>
+            </div>
+            <div class="input-group">	
+                <span class="input-group-addon">End Time</span>
+                <input type="number" class="form-control number" placeholder="Hour" name="hourend" max="23" min="0" value="12" id="hourend" />
+                <input type="number" class="form-control" placeholder="Minute" name="minuteend" max="59" min="0" value="00" step="10" id="minuteend" />
+                <select class="form-control" name="halfdayend">
+                    <option selected="selected">AM</option>
+                    <option>PM</option>
+                </select>
+            </div>
+            
 			<script>
-			 $("#minutestart").change(function(){
-				if($("#minutestart").val() == '0'){
-					$("#minutestart").val('00');
-				}
-			});
+                $("input[type=number]").bind("keydown", function(event) {
+                    var charCode = (event.which) ? event.which : event.keyCode
+                    if (charCode > 31 && (charCode < 48 || charCode > 57))  {
+                        event.preventDefault();
+                    }
+                    else {
+                        var addedChar = String.fromCharCode(charCode);
+                        var oldVal = parseInt($(this).val());
+                        var newVal = parseInt(oldVal + addedChar);
+                        if (isNaN(newVal)) {
+                            oldVal = "";
+                        }
+                        else {
+                            var max = $(this).attr("max");
+                            if (newVal > max) {
+                                oldVal = max;
+                                event.preventDefault();
+                            }
+                            
+                            var min = $(this).attr("min");
+                            if (newVal < min) {
+                                oldVal = min;
+                                event.preventDefault();
+                            }
+                        }
+                        
+                        $(this).val(oldVal);
+                    }
+                });
+                 $("#minutestart").change(function(){
+                    var num = parseInt($("#minutestart").val());
+                    if(num < '10'){
+                        $("#minutestart").val('0'+$number);
+                    }
+                 });
 			 $("#hourstart").change(function(){
-			 var $number = parseInt($("#hourstart").val());
-				if($number < '10'){
+			    var num = parseInt($("#hourstart").val());
+				if(num < '10'){
 					$("#hourstart").val('0'+$number);
 				}
 			});
