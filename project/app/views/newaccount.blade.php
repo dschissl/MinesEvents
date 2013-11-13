766fa529328f422811eeb10ed33899398f1c6077
krobison <krobison@luna.mines.edu>
2013-11-13 13:30:18 -0700

    Added some formatting to new user page

--- project/app/views/newaccount.blade.php
+++ project/app/views/newaccount.blade.php
@@ -1,27 +1,32 @@
 @extends('layouts.master')
 
-{{ HTML::style('css/newaccount.css') }}
 
 @section('title')
     Register
 @endsection
 
 @section('content')
+{{ HTML::style('css/newaccount.css') }}
+<div class="inputpanel">
+	<div class="jumbotron">
+  		<h1>Registering is Free and Simple!</h1>
+  		<p>Users can create new events, manage event groups, view private events, and more!</p>
+	</div>
+
 	 <form action="<?php echo asset('/index.php/accountcreated'); ?>" method="post">
-		<div class="inputpanel">
-				<div class="input-group">	
-					<input type="text" class="form-control" placeholder="Full Name" name="name" required="required" />	
-				</div>
-				<div class="input-group">	
-					<input type="text" class="form-control" placeholder="Email" name="email" required="required" />	
-				</div>
-				<div class="input-group">	
-					<input type="password" class="form-control" placeholder="Password" name="password" required="required" />	
-				</div>
-				<div class="input-group">	
-					<input type="password" class="form-control" placeholder="Confirm Password" name="confirmpassword" required="required" />	
-				</div>
-				<button type="submit" class="btn btn-success" id="createbutton">Create</button>
-		<div>
+			<div class="input-group">	
+				<input type="text" class="form-control" placeholder="Full Name" name="name" required="required" />	
+			</div>
+			<div class="input-group">	
+				<input type="text" class="form-control" placeholder="Email" name="email" required="required" />	
+			</div>
+			<div class="input-group">	
+				<input type="password" class="form-control" placeholder="Password" name="password" required="required" />	
+			</div>
+			<div class="input-group">	
+				<input type="password" class="form-control" placeholder="Confirm Password" name="confirmpassword" required="required" />	
+			</div>
+			<button type="submit" class="btn btn-success" id="createbutton">Register</button>
 	</form>
+<div>
 @endsection
