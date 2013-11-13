eaacbe6e079eac9bfbe494d9bd561006da273a5a
krobison <krobison@luna.mines.edu>
2013-11-13 13:50:23 -0700

    Added some handling for failing login with duplicate email

--- project/app/views/newaccount.blade.php
+++ project/app/views/newaccount.blade.php
@@ -7,12 +7,18 @@
 
 @section('content')
 {{ HTML::style('css/newaccount.css') }}
+
+<?php
+		if ($failed == "true"){
+			echo '<div class="alert alert-danger"><strong>Failed!</strong> An account was found with that email!</div>';
+		}
+	?>
+
 <div class="inputpanel">
 	<div class="jumbotron">
   		<h1>Registering is Free and Simple!</h1>
   		<p>Users can create new events, manage event groups, view private events, and more!</p>
 	</div>
-
 	 <form action="<?php echo asset('/index.php/accountcreated'); ?>" method="post">
 			<div class="input-group">	
 				<input type="text" class="form-control" placeholder="Full Name" name="name" required="required" />	
