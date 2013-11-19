2971cf4b8b04b8370378ebed20613695e0e995d2
krobison <krobison@mines.edu>
2013-11-18 20:21:24 -0700

    Added event detail view

--- project/app/views/eventdetail.blade.php
+++ project/app/views/eventdetail.blade.php
@@ -1,9 +1,38 @@
 @extends('layouts.master')
 
+{{ HTML::style('css/eventdetail.css') }}
+{{ HTML::script('js/jquery-2.0.3.min.js'); }}
+{{ HTML::script('js/googleMaps.js'); }}
+
+
 @section('title')
     Event Detail
 @endsection
 
 @section('content')
-    Detail for event {{ $id }}
+	<?php 
+		$event = DB::table('events')->where('event_id',$id)->first();
+		if ($event->isprivate) {
+			$private = 'Yes';
+		}else{
+			$private = 'No';
+		}
+	?>
+	<div class='eventpanel'>
+		<h1>{{$event->event_name}}</h1>
+		<span class="infobigtext">
+			Where: <span class="infotext">{{$event->location}}</span><span class="tab" />
+			Time: <span class="infotext">{{$event->start_time}} - {{$event->end_time}}</span><span class="tab" />
+			Private: <span class="infotext">{{$private}}</span>
+		</span><hr class="separate">
+		<span class="longtext">
+			{{$event->details}}
+		</span>
+	</div>
+	
+	<div class="mappanel">
+		<div id="map-canvas">
+			<!-- AIzaSyAXTdwWRRhOzQDa1lRcQ7PfKsuhV-XG78A is our API key, also used in the js call -->
+		</div>
+	</div>
 @endsection
